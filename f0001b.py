7a126b8a0f4604f903b702869c2a8fcf38ee097f
SirokiLaci <72387840+SirokiLaci@users.noreply.github.com>
2020-10-19 14:13:28 +0200

    Update f0001b.py

--- f0001b.py
+++ f0001b.py
@@ -1,1 +1,15 @@
-
+#!/usr/bin/env phyton3
+refrén = 'Csillárom, haj!'
+print('Csizmadialegények')
+print('\nCsizmadialegények')
+print(refrén)
+print('Nyolcan ülnek egy lóra')
+print(refrén)
+print('Úgy mennek a jó borra')
+print(refrén)
+print('Parancsolnak kocsmárosnak;')
+print('Mérjen egy decit nyócuknak!')
+print(refrén) 
+print('Azt se tudják kifizetni.')
+print('Az ablakon szekdesnek ki.')
+print(refrén)
